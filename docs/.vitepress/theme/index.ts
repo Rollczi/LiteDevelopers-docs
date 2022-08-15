c19c9fdf4b971ccf556fc309251da786f7eaebed
Rollczi <ndejlich5@gmail.com>
2022-08-15 07:26:10 +0200

    Disable adsite section and increase the max width of content container.

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,5 +1,7 @@
 import DefaultTheme from 'vitepress/theme'
 
+import './patches/disable-adsite-patch.css'
+import './patches/increase-content-container-patch.css'
 import './custom.css'
 import './codetabs.css'
 
