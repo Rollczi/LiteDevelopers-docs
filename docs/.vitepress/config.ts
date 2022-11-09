f2f1a76536bc805d4b395f94dc562ea855c8633e
Rollczi <ndejlich5@gmail.com>
2022-11-09 06:50:35 +0100

    Update vitepress config.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -16,12 +16,74 @@ export default defineConfig({
 
     themeConfig: {
         logo: 'https://i.ibb.co/dWTNW8q/bitmapa.png',
-        nav: nav(),
+        nav: [
+            { text: "LiteCommands", link: "/documentation/litecommands/getting-started" },
+            { text: "LiteSkullAPI", link: "/documentation/liteskullapi/getting-started" }
+        ],
 
         sidebar: {
-            '/documentation/introduction/': configureIntroduction(),
-            '/documentation/litecommands/': configureLiteCommands(),
-            '/documentation/liteskullapi/': configureLiteSkullAPI(),
+
+            // Introduction
+            '/documentation/introduction/': [
+                {
+                    text: 'Introduction',
+                    collapsible: true,
+                    items: [
+                        { text: 'Home', link: '/documentation/introduction/projects' },
+                    ]
+                }
+            ],
+
+            // LiteCommands
+            '/documentation/litecommands/': [
+                {
+                    text: 'Introduction', collapsible: true,
+                    items: [
+                        { text: 'Get started', link: '/documentation/litecommands/getting-started' },
+                        { text: 'Configure Builder', link: '/documentation/litecommands/configure-builder' },
+                    ]
+                },
+
+                {
+                    text: 'Features', collapsible: true,
+                    items: [
+                        { text: '@Route & @Execute', link: '/documentation/litecommands/features/route-and-execute' },
+                        { text: '@Arg', link: '/documentation/litecommands/features/argument' },
+                        { text: '@Arg - Basic Types', link: '/documentation/litecommands/features/argument-basic-types' },
+                        { text: '@Arg - Time', link: '/documentation/litecommands/features/argument-time-types' },
+                        { text: '@Arg - Time Chronology', link: '/documentation/litecommands/features/argument-time-chronology' },
+                        { text: '@Joiner', link: '/documentation/litecommands/features/joiner' },
+                        { text: '@Async & Asynchronously', link: '/documentation/litecommands/features/asynchronously' },
+                    ]
+                },
+
+                {
+                    text: 'Examples', collapsible: true,
+                    items: [
+                        { text: 'Examples - Info', link: '/documentation/litecommands/examples/info' },
+                        { text: 'Bukkit - Teleport', link: '/documentation/litecommands/examples/teleport' },
+                        { text: 'Bukkit - Kick', link: '/documentation/litecommands/examples/kick' },
+                    ]
+                }
+            ],
+
+            // LiteSkullAPI
+            '/documentation/liteskullapi/': [
+                {
+                    text: 'Introduction', collapsible: true,
+                    items: [
+                        { text: 'Get started', link: '/documentation/liteskullapi/getting-started' },
+                    ]
+                },
+                {
+                    text: 'LiteSkullAPI', collapsible: true,
+                    items: [
+                        { text: 'Initialize', link: '/documentation/liteskullapi/initialize' },
+                        { text: 'Basic', link: '/documentation/liteskullapi/basic-api' },
+                        { text: 'CompletableFuture', link: '/documentation/liteskullapi/completable-future-api' },
+                    ]
+                }
+            ],
         },
 
         editLink: {
@@ -40,75 +102,3 @@ export default defineConfig({
         ],
     }
 })
-
-function nav() {
-    return [
-        { text: "LiteCommands", link: "/documentation/litecommands/getting-started" },
-        { text: "LiteSkullAPI", link: "/documentation/liteskullapi/getting-started" }
-    ]
-}
-
-function configureIntroduction() {
-    return [
-        {
-            text: 'Introduction',
-            collapsible: true,
-            items: [
-                { text: 'Home', link: '/documentation/introduction/projects' },
-            ]
-        }
-    ]
-}
-
-function configureLiteCommands() {
-    return [
-        {
-            text: 'Introduction', collapsible: true,
-            items: [
-                { text: 'Get started', link: '/documentation/litecommands/getting-started' },
-                { text: 'Configure Builder', link: '/documentation/litecommands/configure-builder' },
-            ]
-        },
-
-        {
-            text: 'Features', collapsible: true,
-            items: [
-                { text: '@Route & @Execute', link: '/documentation/litecommands/features/route-and-execute' },
-                { text: '@Arg', link: '/documentation/litecommands/features/argument' },
-                { text: '@Arg - Basic Types', link: '/documentation/litecommands/features/argument-basic-types' },
-                { text: '@Arg - Time', link: '/documentation/litecommands/features/argument-time-types' },
-                { text: '@Arg - Time Chronology', link: '/documentation/litecommands/features/argument-time-chronology' },
-                { text: '@Joiner', link: '/documentation/litecommands/features/joiner' },
-                { text: '@Async & Asynchronously', link: '/documentation/litecommands/features/asynchronously' },
-            ]
-        },
-
-        {
-            text: 'Examples', collapsible: true,
-            items: [
-                { text: 'Examples - Info', link: '/documentation/litecommands/examples/info' },
-                { text: 'Bukkit - Teleport', link: '/documentation/litecommands/examples/teleport' },
-                { text: 'Bukkit - Kick', link: '/documentation/litecommands/examples/kick' },
-            ]
-        }
-    ]
-}
-
-function configureLiteSkullAPI() {
-    return [
-        {
-            text: 'Introduction', collapsible: true,
-            items: [
-                { text: 'Get started', link: '/documentation/liteskullapi/getting-started' },
-            ]
-        },
-        {
-            text: 'LiteSkullAPI', collapsible: true,
-            items: [
-                { text: 'Initialize', link: '/documentation/liteskullapi/initialize' },
-                { text: 'Basic', link: '/documentation/liteskullapi/basic-api' },
-                { text: 'CompletableFuture', link: '/documentation/liteskullapi/completable-future-api' },
-            ]
-        }
-    ]
-}
